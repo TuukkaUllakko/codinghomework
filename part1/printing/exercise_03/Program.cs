0a264c573934c0615ca60581f95782b697989ce0
Tuukka Ullakko <tuukka.ullakko@centria.fi>
2020-01-18 14:28:33 +0200

    Printing exercises done

--- part1/printing/exercise_03/Program.cs
+++ part1/printing/exercise_03/Program.cs
@@ -4,9 +4,6 @@ namespace exercise_03
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            // Your code here:
-        }
+        public static void Main(string[] args) => Console.WriteLine("Once upon a time\nI was falling in love\nNow I'm only falling apart");
     }
 }
