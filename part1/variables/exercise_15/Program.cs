83c4c0af486fb6b7cfc8e6dc0909d421de37a4af
Tuukka Ullakko <tuukka.ullakko@centria.fi>
2020-01-19 18:01:00 +0200

    Variable exercises PARTLY done

--- part1/variables/exercise_15/Program.cs
+++ part1/variables/exercise_15/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give a string:");
+      string string1 = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      int integer = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give a double:");
+      double double1 = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine("Give a boolean:");
+      bool boolean = System.Convert.ToBoolean(Console.ReadLine());
+      Console.WriteLine("Your string: " + string1 + "\n" + "Your integer: " + integer + "\n" + "Your double: " + double1 + "\n" + "Your boolean: " + boolean);
+
 
     }
   }
