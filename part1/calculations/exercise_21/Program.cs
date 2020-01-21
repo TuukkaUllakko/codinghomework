aaf0131172820b20952271a988c448811fca9699
Tuukka Ullakko <tuukka.ullakko@centria.fi>
2020-01-21 20:11:11 +0200

    Exercise 20 done

--- part1/calculations/exercise_21/Program.cs
+++ part1/calculations/exercise_21/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //Asks the user for the first integer
+      Console.WriteLine("Give the first number!");
+
+      //User input for the first integer
+      double first = Convert.ToDouble(Console.ReadLine());
+
+      //Asks the user for the second integer
+      Console.WriteLine("Give the second number!");
+
+      //User input for the second integer
+      double second = Convert.ToDouble(Console.ReadLine());
+
+      //Calculates the average as a double
+      Console.WriteLine("The average is " + (first + second) / 2);
 
     }
   }
