5bab5bc27e52609f354afbdb73b6fb852cffd078
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-22 23:00:26 +0200

    Exercise 28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -6,8 +6,24 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the users age
+      Console.WriteLine("How old are you?");
+      //User input for age as an integer
+      int age = Convert.ToInt32(Console.ReadLine());
 
+      //If same or greater than 18, You're an adult!, else You're under age!
+      if (age >= 18)
+      {
+
+        Console.WriteLine("You're an adult!");
+
+      }
+      else
+      {
+
+        Console.WriteLine("You're under age!");
+
+      }
     }
   }
 }
