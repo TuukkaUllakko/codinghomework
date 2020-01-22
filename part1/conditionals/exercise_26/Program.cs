7b7524d2630ffb72b12eb8319d08c58ca76d0b34
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-22 22:44:14 +0200

    Exercise 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the user for integer
+      Console.WriteLine("Give your age:");
+      //User input for the integer
+      int age = Convert.ToInt32(Console.ReadLine());
+
+      //If age less than 1900, prints You're old
+      if (age < 1900)
+      {
+
+        Console.WriteLine("You're old");
+
+      }
 
     }
   }
