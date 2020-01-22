4ca65a9cc4b2fdd6bd4c572bafd086e9ed13ca76
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-22 22:39:45 +0200

    Exercise 25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the user for integer
+      Console.WriteLine("Give a number:");
+      //User input for the integer
+      int year = Convert.ToInt32(Console.ReadLine());
+
+      //If the integer is exactly 1984, prints Orwell
+      if (year == 1984)
+      {
+
+        Console.WriteLine("Orwell");
+
+      }
 
     }
   }
