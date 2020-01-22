c633d55022160895b6ba5ae2936fa400feede516
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-22 23:24:33 +0200

    Exercise 30 done

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -6,8 +6,44 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the user for percent
+      Console.WriteLine("Give your percent [0 - 100]:");
+      //User input for percent
+      int percent = Convert.ToInt32(Console.ReadLine());
 
+      //Gives the user a grade based on the percentage using if, else if and else.
+      if (percent < 0)
+      {
+        Console.WriteLine("Impossible");
+      }
+      else if (percent <= 49)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if (percent <= 59)
+      {
+        Console.WriteLine("Grade: 1");
+      }
+      else if (percent <= 69)
+      {
+        Console.WriteLine("Grade: 2");
+      }
+      else if (percent <= 79)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+      else if (percent <= 89)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (percent <= 100)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      else
+      {
+        Console.WriteLine("Outstanding!");
+      }
     }
   }
 }
