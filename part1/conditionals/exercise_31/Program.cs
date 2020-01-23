600fefb57c943437dcaebdf5c4d3b723b070e37a
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-23 20:48:50 +0200

    Exercise 31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the user for a number
+      Console.WriteLine("Give a number:");
+      //User input for the number
+      int number = Convert.ToInt32(Console.ReadLine());
+
+      //Is the number dividable by 2 (Is the number even?)
+      if ((number % 2) == 0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
 
     }
   }
