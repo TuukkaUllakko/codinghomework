15efccb13cf6500e9869725a1682a97dc4f8ca06
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-24 12:08:20 +0200

    Exercise 39 done

--- part1/repetition/exercise_39/Program.cs
+++ part1/repetition/exercise_39/Program.cs
@@ -6,7 +6,25 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int soi = 0; //soi = sum of integers
+      //Starts the while loop block
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //If blocks analyze the number
+        if (number != 0)
+        {
+          soi = soi + number;
+          
+        }
+        else
+        {
+          Console.WriteLine("Total sum of numbers: " + soi);
+          break;
+        }
+      }
     }
   }
 }
