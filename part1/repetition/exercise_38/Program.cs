b0085cf25dd09155ae73987c8041387fc455942b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-24 12:01:27 +0200

    Exercise 38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -6,7 +6,28 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int smn = 0; //smn = sum of negatives
+      //Starts the loop block
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //If blocks come in to analyze the answer
+        if (number < 0)
+        {
+          smn = smn + 1;
+
+        }
+        else if (number == 0)
+        {
+          Console.WriteLine("Total amount of negative numbers: " + smn);
+          break;
+
+        }
+
+
+      }
 
     }
   }
