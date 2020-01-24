00155e274476d3600033a80330f2c9b45c41d5d4
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-24 11:26:27 +0200

    Exercise 37 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -6,7 +6,33 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Creates the "loop block"
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //Creates an if-block within the loop if 0, quit
+        if (number == 0)
+        {
+          break;
+
+        }
+
+        //If negative, print "That is negative"
+        else if (number < 0)
+        {
+          Console.WriteLine("That is negative");
+
+        }
+        else
+        {
+          Console.WriteLine(Math.Pow(number, 2)); //Math.Pow used for exponent, for raising to power!
+
+        }
+
+
+      }
 
     }
   }
