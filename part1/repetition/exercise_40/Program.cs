9873cd13fc8c7742571d0303e66b5f85c976454e
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-24 12:17:36 +0200

    Exercise 40 done, part 1 complete!

--- part1/repetition/exercise_40/Program.cs
+++ part1/repetition/exercise_40/Program.cs
@@ -7,7 +7,28 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int aon = 0; //amount of numbers
+      int soi = 0; //sum of integers
+      //Starts the while loop block
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //If comes in to analyze the answer and process it accordingly
+        if (number != 0)
+        {
+          aon = aon + 1;
+          soi = soi + number;
+
+        }
+        else
+        {
+          Console.WriteLine("Total sum of numbers: " + soi);
+          Console.WriteLine("Total amount of numbers: " + aon);
+          break;
+        }
+      }
     }
   }
 }
