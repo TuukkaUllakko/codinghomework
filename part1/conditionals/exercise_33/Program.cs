95e9fc6d30959aa4062158354745a6be77750a21
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-24 11:00:26 +0200

    Exercise 33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      //Asks the user for the first string
+      Console.WriteLine("Give the first string:");
+      //User input for the first string
+      string first = Console.ReadLine();
+      //Asks the user for the second string
+      Console.WriteLine("Give the second string:");
+      //User input for the second string
+      string second = Console.ReadLine();
+
+      //If same, print Echo!, if different, print Nope!
+      if (first == second)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
 
     }
   }
