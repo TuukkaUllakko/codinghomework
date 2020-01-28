5dfbaebc11b2bec9e652541aff7574c7cdad273a
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-28 17:12:57 +0200

    Exercise 42 done!

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      //User input for the two integers
+      int first = Convert.ToInt32(Console.ReadLine());
+      int second = Convert.ToInt32(Console.ReadLine());
+
+      //Sums the numbers together and prints out the square root of the result as a double
+      double answer = Math.Sqrt(first + second);
+      Console.WriteLine(answer);
     
     }
   }
