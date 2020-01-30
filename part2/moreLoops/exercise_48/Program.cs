8366bfe0be421220a3be98e031d6110c40124785
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-30 22:23:30 +0200

    Exercise 48 done!

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -7,7 +7,51 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give numbers:"); 
+      
+      double sum = 0;
+      double amount = 0;
+      double odd = 0;
+      double even = 0;
+
+      
+      
+      while (true)
+      {
+        double number = Convert.ToDouble(Console.ReadLine());
+        
+        if (number != -1)
+        {
+          sum = sum + number;
+          amount++;
+          
+          if (number % 2 == 0)
+          {
+            even = even + 1;
+
+          }
+          else
+          {
+            odd = odd + 1;
+
+          }
+
+        }
+        
+        else if (number == -1)
+        {
+          Console.WriteLine("Thx! Bye!");
+          Console.WriteLine("Sum: " + sum);
+          Console.WriteLine("Numbers: " + amount);
+          Console.WriteLine("Average: " + (sum / amount));
+          Console.WriteLine("Even: " + even);
+          Console.WriteLine("Odd: " + odd);
+          break;
+        }
+        
+        
+
+      }
  
     }
   }
