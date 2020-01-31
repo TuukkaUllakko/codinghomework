e30090137cad156fc7470eae4d4cfd838e853c1c
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-31 07:47:30 +0200

    Exercise 50 done!

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,11 +7,23 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      int times = Convert.ToInt32(Console.ReadLine());
+      int ender = 1;
+
+      while (ender <= times)
+      {
+        PrintPhrase();
+        ender++;
+
+      }
  
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
 
     }
   }
