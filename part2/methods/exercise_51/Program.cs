bf901cebe6b946840dbef00ae76336eea79a4c4b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-01-31 08:24:08 +0200

    Exercise 51 done!

--- part2/methods/exercise_51/Program.cs
+++ part2/methods/exercise_51/Program.cs
@@ -7,11 +7,34 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
+      PrintUntilNumber(8);
 
 
     }
 
     // Write your method here:
+    public static void PrintUntilNumber(int number)
+    {
+      int ladder = 1;
+      
+      while (true)
+      {
+        if (ladder <= number)
+        {
+          Console.WriteLine(ladder);
+          ladder++;
+          continue;
+
+        }
+        else
+        {
+          break;
+
+        }
+      }
+      
+
+    }
 
   }
 }
