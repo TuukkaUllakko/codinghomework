e5fd62408178dccda002423b23306f14bb0fca02
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-01 22:06:30 +0200

    Exercise 60 done!

--- part2/methods/exercise_60/Program.cs
+++ part2/methods/exercise_60/Program.cs
@@ -6,6 +6,7 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
+      PrintTriangle(5);
 
     }
 
@@ -17,20 +18,40 @@ namespace exercise_60
       // call the print command n times
       // in the end print a line break with the comand
       // Console.WriteLine("");
+      for (int stars = 1; stars <= number; stars++)
+      {
+        Console.Write("*");
+
+      }
+        Console.WriteLine("");
+      
     }
 
     public static void PrintSquare(int size)
     {
+    
+      for (int square = 1; square <= size; square++)
+      {
+        PrintStars(size);
+        
+      }
 
     }
 
     public static void PrintRectangle(int width, int height)
     {
+      for (int h = 1; h <= height; h++)
+      PrintStars(width);
 
     }
 
     public static void PrintTriangle(int size)
     {
+      for (int t = 1; t <= size; t++)
+      {
+        PrintStars(t);
+
+      }
 
     }
   }
