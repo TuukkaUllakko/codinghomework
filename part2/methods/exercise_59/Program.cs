210db9a0b7f2a311ab2bc78a26fc21b4fac0449c
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-01 20:00:30 +0200

    Exercise 59 done!

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -11,6 +11,27 @@ namespace exercise_59
     }
 
     // Write your method here:
+    public static int Greatest(int first, int second, int third)
+    {
+      if (first > second && first > third)
+      {
+        return first;
+
+      }
+      
+      else if (second > first && second > third)
+      {
+        return second;
+
+      }
+
+      else
+      {
+        return third;
+
+      }
+
+    }
 
   }
 }
