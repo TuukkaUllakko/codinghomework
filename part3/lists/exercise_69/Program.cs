a2f97dc911bd70ea104ca680d30ac3aba5fca84b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-05 11:44:36 +0200

    Exercise 69 done!

--- part3/lists/exercise_69/Program.cs
+++ part3/lists/exercise_69/Program.cs
@@ -17,6 +17,20 @@ namespace exercise_69
         }
         list.Add(input);
       }
+      Console.WriteLine("From where?");
+      int start = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Where to?");
+      int end = Convert.ToInt32(Console.ReadLine());
+
+      foreach (int number in list)
+      {
+        if ((start <= number) && (end >= number))
+        {
+          Console.WriteLine(number);
+
+        }
+
+      }
 
     }
   }
