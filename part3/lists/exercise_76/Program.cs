4dbf5d4be6c858a05352d0b1035b0038d6ad7b50
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-06 23:30:05 +0200

    Exercise 76 done!

--- part3/lists/exercise_76/Program.cs
+++ part3/lists/exercise_76/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_76
   {
     public static void Main(string[] args)
     {
+      List<int> numbers = new List<int>();
+
+      numbers.Add(4);
+      numbers.Add(5);
+      numbers.Add(6);
+      Console.WriteLine(Sum(numbers));
+      numbers.Add(10);
+      Console.WriteLine(Sum(numbers));
+
+    }
+    public static int Sum(List<int> numbers)
+    {
+      int sum = 0;
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        sum = sum + numbers[i];
+
+      }
+      return sum;
 
     }
 
