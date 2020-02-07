dc3b26243f07fc2be4f76c58419b8fae42271c7f
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-07 18:46:06 +0200

    Exercise 79 done!

--- part3/arrays/exercise_79/Program.cs
+++ part3/arrays/exercise_79/Program.cs
@@ -21,7 +21,22 @@ namespace exercise_79
       Console.WriteLine("Search for?");
       int searching = Convert.ToInt32(Console.ReadLine());
 
-      // Implement the search functionality here
+      bool found = false;
+
+      for (int index = 0; index < array.Length; index++)
+      {
+        if (array[index] == searching)
+        {
+          Console.WriteLine(searching + " is at index " + index + ".");
+          found = true;
+        }
+
+      }
+      if (found == false)
+      {
+        Console.WriteLine(searching + " was not found.");
+
+      }
 
     }
 
