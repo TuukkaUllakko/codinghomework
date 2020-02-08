09a4a14a59e6ad419cc983c58a23246dd09dfb47
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-08 14:14:15 +0200

    Exercise 85 done?

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,7 +7,35 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string username = Console.ReadLine();
+      if (username == "alex")
+      {
+        Console.WriteLine("Enter password:");
+        string passalex = Console.ReadLine();
+        if (passalex == "sunshine")
+        {
+          Console.WriteLine("You have successfully logged in!");
+        }
+        else
+        {
+          Console.WriteLine("Incorrect username or password!");
+        }
 
+      }
+      if (username == "emma")
+      {
+        Console.WriteLine("Enter password:");
+        string passemma = Console.ReadLine();
+        if (passemma == "haskell")
+        {
+          Console.WriteLine("You have successfully logged in!");
+        }
+        else
+        {
+          Console.WriteLine("Incorrect username or password!");
+        }
+      }
 
     }
   }
