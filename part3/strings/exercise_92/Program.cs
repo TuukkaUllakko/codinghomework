12762372e738b8d5abc00452a57c733ff5a25ccc
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-08 19:47:59 +0200

    Exercise 92 done!

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,31 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
-
+      string longname = "empty";
+      int longest = 0;
+      int highest = 0;
+      while (true) //Starts a loop asking for questions.
+      {
+        string input = Console.ReadLine(); //User input
+        if (input == "") //Stops the loop questioning from user if enter is simply pressed with no input.
+        {
+          break;
+        }
+        string[] parts = input.Split(","); //Splits the names from ages into separate pieces.
+        int length = input.Length;
+        int age = 2020 - Convert.ToInt32(parts[1]); //Turns the year of birth into integer and calculates the age. That's then the variable "age".
+        if (length > longest) //Replaces the previous longest name length and the previous name if the new name length is longer.
+        {
+          longest = length;
+          longname = parts[0];
+        }
+        if (age > highest) //Uses the age from "int age". If it's bigger than the previous highest age, the highets age gets replaced by it.
+        {
+          highest = age;
+        }
+      }
+      Console.WriteLine("Longest name: " + longname); //Prints the result
+      Console.WriteLine("Highest age: " + highest);   //Prints the result
     }
   }
 }
