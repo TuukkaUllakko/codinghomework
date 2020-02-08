c4348494993338c3e8c103a12c6c6e0cc17f4050
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-08 16:05:10 +0200

    Exercise 87 done!

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,6 +7,23 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] parts = input.Split(" ");
+        
+        for (int i = 0; i < parts.Length; i++)
+        {
+          if (parts[i].Contains("av"))
+          {
+            Console.WriteLine(parts[i]);
+          }
+        }
+      }
 
     }
 
