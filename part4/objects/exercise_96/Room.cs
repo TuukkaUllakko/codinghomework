8f82cfb4be4ef799d153b0069a7a88cc46d353d3
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-12 12:19:29 +0200

    Exercise 96 done!

---
+++ part4/objects/exercise_96/Room.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace exercise_96
+{
+
+    public class Room
+    {
+        private string code;
+        private int seats;
+
+        public Room(string classCode, int numberOfSeats)
+        {
+            this.code = code;
+            this.seats = 0;
+
+        }
+
+    }
+
+
+
+}
