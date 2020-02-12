06debdd7310810a95c77c4cf723c9dab109b5b1a
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-12 11:46:08 +0200

    Exercise 93 done!

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,6 +8,11 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account tuukkasAccount = new Account("Tuukka's account", 100.0);
+      
+      tuukkasAccount.Deposit(20);
+
+      Console.WriteLine(tuukkasAccount.balance);
 
     }
   }
