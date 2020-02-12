1c3a3aa1c89482581f5a1a4b55238a0e09d22cb2
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-12 13:13:29 +0200

    Exercise 98 done!

--- part4/objects/exercise_98/Program.cs
+++ part4/objects/exercise_98/Program.cs
@@ -8,6 +8,9 @@ namespace exercise_98
     {
       // Create Product.cs and follow the instructions.
       // You can test your class with this.
+      Product tomato = new Product("tomato", 0.99, 7);
+
+      tomato.PrintProduct();
       
 
     }
