52f93f7c51594537ca319ec8ae678e1605aaee9d
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-16 12:55:12 +0200

    Exercise 115 done!

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+      while (true)
+      {
+        String input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        else
+        {
+          int number = Convert.ToInt32(input);
+          Console.WriteLine(Math.Pow(number, 3));
+        }
+      }
     }
   }
 }
