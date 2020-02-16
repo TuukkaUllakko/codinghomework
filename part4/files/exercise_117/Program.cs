62a559fee8b6a6a86732107115e92341ce20483a
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-16 13:03:31 +0200

    Exercise 117 done!

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,6 +8,12 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
+      Console.WriteLine("Which file should have its contents printed?");
+      String input = Console.ReadLine();
+
+      String text = File.ReadAllText(input);
+
+      Console.WriteLine(text);
 
       
     }
