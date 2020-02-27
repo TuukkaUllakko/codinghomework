e6c2a57efe37e9019cf20196adec0b81d9c7f41b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-02-28 00:04:17 +0200

    Exercise 131 done!

--- part5/references/exercise_131/Program.cs
+++ part5/references/exercise_131/Program.cs
@@ -3,47 +3,47 @@ using System.Collections.Generic;
 
 namespace exercise_131
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<Item> items = new List<Item>();
-
-      // Ask for input as shown in the exercise.
-      while (true)
-      {
-        Console.WriteLine("Identifier? (empty will stop):");
-        string identifier = Console.ReadLine();
-      
-        if (identifier == "")
+        public static void Main(string[] args)
         {
-         break;
-        }
-        
-        Console.WriteLine("Name? (empty will stop):");
-        string name = Console.ReadLine();
+            List<Item> items = new List<Item>();
 
-        Item item = new Item(identifier, name);
-        if (items.Contains(item))
-        {
-          continue;
-        }
-        else
-        {
-          items.Add(item);
-        }
-      }
-      
+            // Ask for input as shown in the exercise.
+            while (true)
+            {
+                Console.WriteLine("Identifier? (empty will stop):");
+                string identifier = Console.ReadLine();
+
+                if (identifier == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Name? (empty will stop):");
+                string name = Console.ReadLine();
+
+                Item item = new Item(identifier, name);
+                if (items.Contains(item))
+                {
+                    continue;
+                }
+                else
+                {
+                    items.Add(item);
+                }
+            }
 
 
-      // The end printing is ready, don't touch this
-      Console.WriteLine("==Items==");
-      foreach (Item item in items)
-      {
-        Console.WriteLine(item);
-      }
+
+            // The end printing is ready, don't touch this
+            Console.WriteLine("==Items==");
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
 
     }
-
-  }
 }
