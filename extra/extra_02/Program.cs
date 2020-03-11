3a10d7b5737017b246d6bf12a79476cb8ea86843
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-11 13:10:26 +0200

    Extra 2 done!

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,7 +7,21 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
+      int largest = Int32.MinValue;
 
+      Console.WriteLine("Give three numbers:");
+
+      for (int i = 0; i < 3; i++)
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if (number > largest)
+        {
+          largest = number;
+        }
+      }
+
+      Console.WriteLine("Largest: " + largest);
     }
   }
 }
