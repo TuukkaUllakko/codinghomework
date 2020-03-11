9137633281bfa6da2858394248fdb93e1c8e4667
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-11 13:29:09 +0200

    Extra 3 done!

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -7,7 +7,25 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
+      int sum = 0;
+      int total = 1;
 
+      Console.WriteLine("How many numbers?");
+
+      int times = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Give " + times + " numbers:");
+
+      for (int i = 0; i < times; i++)
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+        sum += number;
+        total *= number;
+      }
+
+      Console.WriteLine("Their sum: " + sum +
+      "\nTheir total: " + total +
+      "\nTheir average: " + (double)sum / times);
     }
   }
 }
