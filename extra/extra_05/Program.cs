82043ab37702536fe2ee3967615f490903b6ac45
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-13 13:16:46 +0200

    Extra 5 done!

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
@@ -7,7 +8,27 @@ namespace extra_05
     public static void Main(string[] args)
     {
       // Add your code here:
+      for (int i = 1; i <= 5; i++)
+      {
+        Console.WriteLine(i);
+      }
 
+      List<string> words = new List<string>();
+      Console.WriteLine("Give words, 'end' quits:");
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "end")
+        {
+          break;
+        }
+        words.Add(input);
+      }
+
+      foreach (string word in words)
+      {
+        Console.WriteLine(word);
+      }
     }
   }
 }
