d15d2b2c7a00574f446e43003638765e3455b5db
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-13 13:29:26 +0200

    Extra 6 done!

--- extra/extra_06/Program.cs
+++ extra/extra_06/Program.cs
@@ -7,7 +7,18 @@ namespace extra_06
     public static void Main(string[] args)
     {
       // Add your code here:
+      int input1 = Convert.ToInt32(Console.ReadLine());
+      int input2 = Convert.ToInt32(Console.ReadLine());
 
+      int sum = input1 + input2;
+      if (sum < 0)
+      {
+        Console.WriteLine("0");
+      }
+      else
+      {
+      Console.WriteLine(Math.Sqrt(sum));
+      }
     }
   }
 }
