896a5498f87f9eb69d7f72f8facbc8c36de49b3b
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-14 16:40:59 +0200

    Extra 11 done!

--- extra/extra_11/Program.cs
+++ extra/extra_11/Program.cs
@@ -7,7 +7,16 @@ namespace extra_11
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give a sentence:"); //Asks the user for a sentence
 
+      string sentence = Console.ReadLine();  //User input for the sentence
+
+      string[] pieces = sentence.Split(" "); //Cuts away the spaces in the sentence and puts the parts in an array called pieces
+
+      foreach (string piece in pieces) //Prints out every piece in the pieces array
+      {
+        Console.WriteLine(piece);
+      }
     }
   }
 }
