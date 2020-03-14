df66a035611c5bb55342e0c669bb69b992822cc0
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-14 16:32:07 +0200

    Extra 10 done!

--- extra/extra_10/Program.cs
+++ extra/extra_10/Program.cs
@@ -7,7 +7,19 @@ namespace extra_10
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      int[] array = {5, 1, 3, 4, 2};
+      PrintArrayInStars(array);
+    }
+    public static void PrintArrayInStars(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int stars = 0; stars < array[i]; stars++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");
+      }
     }
   }
 }
