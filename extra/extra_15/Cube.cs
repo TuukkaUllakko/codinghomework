52d96375df05dd42d3219de29d5b412b1b5ff593
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-03-19 21:45:19 +0200

    Extra 15 done!

---
+++ extra/extra_15/Cube.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+namespace extra_15
+{
+    public class Cube
+    {
+        private int edgeLength; //Variables
+        private int volume;
+        public Cube (int edgeLength) //Constructor
+        {
+            this.edgeLength = edgeLength;
+        }
+        public int Volume() //Methods
+        {
+            this.volume = this.edgeLength * this.edgeLength * this.edgeLength;
+            return this.volume;
+        }
+        public override string ToString()
+        {
+            return "The length of the edge is " + this.edgeLength + " and the volume " + Volume();
+        }
+    }
+}
