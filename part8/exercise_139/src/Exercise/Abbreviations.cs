fb3e779345e1ba9fc044e09de56ee68a33b895a0
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-02 16:23:29 +0300

    Exercise 139 done!

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,33 @@ namespace Exercise
 {
   public class Abbreviations
   {
+    private Dictionary<string, string> abbreviations;
+    public Abbreviations()
+    {
+      this.abbreviations = new Dictionary<string, string>();
+    }
 
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbreviations.Add(abbreviation, explanation);
     }
 
     public bool HasAbbreviation(string abbreviation)
     {
+      if (this.abbreviations.ContainsKey(abbreviation))
+      {
+        return true;
+      }
       return false;
     }
 
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if (HasAbbreviation(abbreviation))
+      {
+        return this.abbreviations[abbreviation];
+      }
+      return "not found";
     }
   }
 }
