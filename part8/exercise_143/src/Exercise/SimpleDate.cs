60e15945458884d306d89212c603ad52ef898c3f
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-09 14:05:45 +0300

    Exercise 143 done!

--- part8/exercise_143/src/Exercise/SimpleDate.cs
+++ part8/exercise_143/src/Exercise/SimpleDate.cs
@@ -38,14 +38,24 @@ namespace Exercise
 
     public override bool Equals(object compared)
     {
-      // DO SOMETHING HERE
-      return false;
+      if (this == compared)
+      {
+        return true;
+      }
+      if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+      {
+        return false;
+      }
+      else
+      {
+        SimpleDate comparedDate = (SimpleDate)compared;
+        return this.day == comparedDate.day && this.month == comparedDate.month && this.year == comparedDate.year;
+      }
     }
 
     public override int GetHashCode()
     {
-      // DO SOMETHING HERE
-      return -1;
+      return this.day + this.month + this.year.GetHashCode();
     }
   }
 }
