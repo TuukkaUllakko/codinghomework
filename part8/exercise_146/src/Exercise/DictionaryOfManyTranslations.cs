6e0e85e3069bf1b3899d5adbcaf54373474b4a2c
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-11 00:58:48 +0300

    Exercise 146 done!

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -1,23 +1,38 @@
+using System;
 using System.Collections.Generic;
 
 namespace Exercise
 {
   public class DictionaryOfManyTranslations
   {
+    private Dictionary<string, List<string>> dictionary;
+    public DictionaryOfManyTranslations()
+    {
+      this.dictionary = new Dictionary<string, List<string>>();
+    }
 
     public void Add(string word, string translation)
     {
+      if (!this.dictionary.ContainsKey(word))
+      {
+        this.dictionary.Add(word, new List<string>());
+      }
+        this.dictionary[word].Add(translation);
 
     }
 
     public List<string> Translate(string word)
     {
-      return new List<string>();
+      if (!this.dictionary.ContainsKey(word))
+      {
+        this.dictionary.Add(word, new List<string>());
+      }
+      return this.dictionary[word];
     }
 
     public void Remove(string word)
     {
-
+      this.dictionary.Remove(word);
     }
   }
 }
