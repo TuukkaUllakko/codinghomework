bbe3448722f2ce2905a5a79a268ab5346adea917
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-14 22:22:28 +0300

    Exercise 149 done!

--- part9/exercise_149/src/Exercise/Program.cs
+++ part9/exercise_149/src/Exercise/Program.cs
@@ -2,11 +2,14 @@ using System;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-
+        public static void Main(string[] args)
+        {
+            Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+            Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+            Console.WriteLine(ada);
+            Console.WriteLine(esko);
+        }
     }
-  }
 }
