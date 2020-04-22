84a42a3f13f4c587f47c5029306ea0a62e71f001
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-22 13:37:08 +0300

    Exercise 158 done!

--- part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
+++ part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
@@ -8,10 +8,38 @@ namespace Exercise
     private List<Book> books;
     public TextInterface()
     {
+      this.books = new List<Book>();
     }
 
     public void Start()
     {
+      while (true)
+      {
+        Console.WriteLine("Input the name of the book, empty stops");
+        string userInputName = Console.ReadLine();
+
+        if (userInputName == "")
+        {
+          break;
+        }
+
+        Console.WriteLine("Input the age recommendation:");
+        string userInputAge = Console.ReadLine();
+
+        if (userInputAge == "")
+        {
+          break;
+        }
+
+        int age = Convert.ToInt32(userInputAge);
+        this.books.Add(new Book(userInputName, age));
+      }
+      Console.WriteLine(books.Count + " books in total.");
+      Console.WriteLine("\nBooks:");
+
+      books.Sort();
+      
+      this.books.ForEach(Console.WriteLine);
     }
   }
 }
