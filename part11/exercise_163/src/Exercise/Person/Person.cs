880cc8d30f9d7a6794c33f2f607dcc2e8b8730b6
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-25 00:02:57 +0300

    Exercise 163 done!

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -9,6 +9,14 @@ namespace Exercise
 
     public Person(string name, int age)
     {
+      if (name == null || name == "" || name.Length > 40)
+      {
+        throw new ArgumentException("Give a real name.");
+      }
+      if (age < 0 || age > 120)
+      {
+        throw new ArgumentException("Give an age between 0 and 120,");
+      }
       this.name = name;
       this.age = age;
     }
