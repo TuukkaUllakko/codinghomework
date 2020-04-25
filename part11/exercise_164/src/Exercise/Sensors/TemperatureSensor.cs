521381ba96e789550fdb3eb1a1f75138ce1649b3
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-04-25 03:40:26 +0300

    Exercise 164 done!

--- part11/exercise_164/src/Exercise/Sensors/TemperatureSensor.cs
+++ part11/exercise_164/src/Exercise/Sensors/TemperatureSensor.cs
@@ -3,26 +3,42 @@ namespace Exercise
   using System;
   public class TemperatureSensor : Sensor
   {
-
+    private bool onOrOff = false;
+    public TemperatureSensor()
+    {
+      this.onOrOff = false;
+    }
     public bool IsOn()
     {
-      return false;
+      return onOrOff;
     }
 
 
     public void SetOn()
     {
+      this.onOrOff = true;
     }
 
 
     public void SetOff()
     {
+      this.onOrOff = false;
     }
 
 
     public int Read()
     {
-      return 0;
+      int temp = 0;
+      Random temperature = new Random();
+      temp = temperature.Next(-30, 30);
+      if (IsOn())
+      {
+      return temp;
+      }
+      else
+      {
+        throw new InvalidOperationException("Turn on the temperature sensor!");
+      }
     }
   }
 }
